ad68b7bbd9562019d9fdc9c4fb3b222ad7c0301f
Michelle Ruiter <mruiter4@gmail.com>
2020-06-28 22:18:13 -0700

    HTML and JS setup

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 "use strict"
 
+const inhaleTime = 3000;
+const holdTime = 2000;
+const totalTime = 9000;
+
+const textPromt = document.getElementById('text-prompts')
+const container = document.getElementById('container')
+
+breathe();
+
+function breathe() {
+    textPromt.innerText = 'Breathe';
+    //container.className = 'grow';
+
+    setTimeout(() => {
+        textPromt.innerText = 'Hold';
+        
+        setTimeout(() => {
+            textPromt.innerText = 'Exhale';
+            //container.className = 'shrink';
+        }, holdTime);
+    }, inhaleTime);
+}
+
+setInterval(breathe, totalTime);
